bfe1d3c1eceda54b843caa040d75b629aa042c0c
NjeriMarion <njeriwmarion@gmail.com>
2023-03-29 06:50:52 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,46 @@
+
+// Find the last element of the following arrays.
+// arr1 = [3,7,34,90,12]
+// arr2 = [true, "green", "where",12,56]
+
+let arr1 = [3,7,34,90,12];
+let arr4 = arr1[arr1.length-1];
+console.log([arr4]);
+
+let arr2 = [true, "green", "where",12,56];
+let mix = arr2[arr2.length-1];
+console.log(mix);
+
+
+
+// Write a JS program that will join the following array elements into a string
+let mypets =[" cow","python","snake","dog"];
+let we = mypets.join();
+console.log(we);
+
+
+
+// write a JS script to sort the following array items
+// var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+var arr3 =[-5,9,5,3,2,-3,6,8,4,1];
+let nums =arr3.sort((a,b)=>a -b);
+console.log(nums);
+
+// write a program to remove duplicates from the following array.
+// Console the array without duplicates, and console another array that only contains the duplicates
+// var arr = ["apple", "mango", "apple",
+//             "orange", "mango", "mango"];
+var arr =["apple","mango","apple","orange","mango","mango",];
+function removeFruit(arr){
+    return[...new Set(arr)];
+
+
+}
+console.log(removeFruit(arr));
+
+function findDuplicates(arr){
+    return arr.filter((number,index)=>arr.indexOf(number) !==index)
+
+}
+console.log(findDuplicates(arr));
+
